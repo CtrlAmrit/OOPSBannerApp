408514e045e359796200ba48d3e0389a09ee3a2f
Amrit <Amrit.arnav268@gmail.com>
2026-03-02 00:55:19 +0530

    UC4: Refactored banner using String arrays and loops

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,41 +1,55 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC4: Using Arrays and Loops to render banner
+ *
+ * @author Lynxx
+ * @version 1.0
+ */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        printBanner();
-    }
 
-    public static void printBanner() {
+        String[] O = {
+                " ******** ",
+                "*        *",
+                "*        *",
+                "*        *",
+                "*        *",
+                "*        *",
+                " ******** "
+        };
 
-        String O1 = " ******** ";
-        String O2 = "*        *";
-        String O3 = "*        *";
-        String O4 = "*        *";
-        String O5 = "*        *";
-        String O6 = "*        *";
-        String O7 = " ******** ";
+        String[] P = {
+                "********  ",
+                "*       * ",
+                "*       * ",
+                "********  ",
+                "*         ",
+                "*         ",
+                "*         "
+        };
 
-        String P1 = "********  ";
-        String P2 = "*       * ";
-        String P3 = "*       * ";
-        String P4 = "********  ";
-        String P5 = "*         ";
-        String P6 = "*         ";
-        String P7 = "*         ";
+        String[] S = {
+                " ******** ",
+                "*         ",
+                "*         ",
+                " ******** ",
+                "         *",
+                "         *",
+                " ******** "
+        };
 
-        String S1 = " ******** ";
-        String S2 = "*         ";
-        String S3 = "*         ";
-        String S4 = " ******** ";
-        String S5 = "         *";
-        String S6 = "         *";
-        String S7 = " ******** ";
+        // Create banner lines using concatenation
+        String[] banner = new String[7];
 
-        System.out.println(O1 + " " + O1 + " " + P1 + " " + S1);
-        System.out.println(O2 + " " + O2 + " " + P2 + " " + S2);
-        System.out.println(O3 + " " + O3 + " " + P3 + " " + S3);
-        System.out.println(O4 + " " + O4 + " " + P4 + " " + S4);
-        System.out.println(O5 + " " + O5 + " " + P5 + " " + S5);
-        System.out.println(O6 + " " + O6 + " " + P6 + " " + S6);
-        System.out.println(O7 + " " + O7 + " " + P7 + " " + S7);
+        for (int i = 0; i < 7; i++) {
+            banner[i] = O[i] + " " + O[i] + " " + P[i] + " " + S[i];
+        }
+
+        // Enhanced for-loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
